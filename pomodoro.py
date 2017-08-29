dfb567ce023207fe77dd1fb0654d31b051057caa
Stenio Araujo <stenioaraujo@lsd.ufcg.edu.br>
2017-08-29 15:22:36 -0300

    Fixing accumulating multiple terminals

--- pomodoro.py
+++ pomodoro.py
@@ -10,28 +10,45 @@ def minutes_to_seconds(minutes):
 	"""
 	return 60 * minutes
 
+
+TIME_TO_WORK = minutes_to_seconds(25)
+TIME_TO_REST = minutes_to_seconds(5)
+END_OF_POMODORO = minutes_to_seconds(30)
+
+
+def open_terminal(python_script, time_to_close=5):
+        """Open a new terminal with the python script passed
+
+        :param python_script: The path of the python script to be
+        executed
+        :param time_to_close: the number of seconds before closing
+        the new terminal
+        """
+
+        os.system("x-terminal-emulator -e 'bash -c \"python %s  %d; exec bash\"'" % (python_script, time_to_close))
+
+
+def work(time_to_work=TIME_TO_WORK):
+    open_terminal("print_work.py", time_to_work);
+    time.sleep(time_to_work);
+
+
+def rest(time_to_rest=TIME_TO_REST):
+    open_terminal("print_rest.py", time_to_rest)
+    time.sleep(time_to_rest)
+
+
 print ("Pomodoro started")
 
-time_to_work = minutes_to_seconds(25)
-time_to_rest = minutes_to_seconds(5)
-end_of_pomodoro = minutes_to_seconds(30)
-
 new_pomodoro = True;
-
 while (new_pomodoro):
 	for i in range(0, 3):
-		time.sleep(time_to_work);
-		os.system("x-terminal-emulator -e 'bash -c \"python print_rest.py; exec bash\"'")
-		time.sleep(time_to_rest);
-		os.system("x-terminal-emulator -e 'bash -c \"python print_work.py; exec bash\"'")
-
-	time.sleep(time_to_work);
-	os.system("x-terminal-emulator -e 'bash -c \"python print_rest.py; exec bash\"'")
-	time.sleep(end_of_pomodoro);
-	os.system("x-terminal-emulator -e 'bash -c \"python print_work.py; exec bash\"'")
+                work()
+                rest()
+        work()
+        rest(END_OF_POMODORO)
 	
 	new_pomodoro = raw_input("Do you want to start a new pomodoro? (y/n) ")
-	
 	if new_pomodoro == "y":
 		new_pomodoro = True;
 	else:
