5411526fa6a735643aed9759aea1476616389492
Felipe Marinho <felipevm97@gmail.com>
2017-10-23 13:11:23 -0300

    adding python3 support

--- print_work.py
+++ print_work.py
@@ -4,23 +4,23 @@ import os
 import sys
 import time
 
-print ""
-print ""
-print ""
-print ""
-print ""
-print ".¸¸.*♡*.¸¸.*☆*¸.*♡*.¸¸.*☆*.¸¸.*♡*.¸¸.*☆*"
-print ""
+print("")
+print("")
+print("")
+print("")
+print("")
+print(".¸¸.*♡*.¸¸.*☆*¸.*♡*.¸¸.*☆*.¸¸.*♡*.¸¸.*☆*")
+print("")
 
-print "                Time to work            "
+print("                Time to work            ")
 
-print ""
-print ".¸¸.*♡*.¸¸.*☆*¸.*♡*.¸¸.*☆*.¸¸.*♡*.¸¸.*☆*"
-print ""
-print ""
-print ""
-print ""
-print ""
+print("")
+print(".¸¸.*♡*.¸¸.*☆*¸.*♡*.¸¸.*☆*.¸¸.*♡*.¸¸.*☆*")
+print("")
+print("")
+print("")
+print("")
+print("")
 
 time.sleep(int(sys.argv[1]))
 os.kill(os.getppid(), 9)
